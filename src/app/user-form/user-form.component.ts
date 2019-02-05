be2a6a05b04c668e32fb526b5b50d45f1018c6d3
EspeIgira <Mapapuro2>
2019-02-05 14:38:00 +0200

    created service request

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,43 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+
+
 import {UserApi} from '../user-api';
 import {environment} from '../../environments/environment'
-//import {UserRequestService} from '../user-http/user-request.service'
+import {UserRequestService} from '../user-http/user-request.service'
 
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
+  providers:[UserRequestService], 
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit{
 
-
-  newUserApi=new UserApi("",0,0,0,"","","");
   user:UserApi;
 
 
-  constructor(private http:HttpClient) { }
+  constructor(private userService:UserRequestService) {
+   
+   }
 
   ngOnInit() {
 
-    interface ApiResponse{
-      avatar_url:string;
-      name:string;
-      public_repospublic:number;
-      followers:number;
-      following:number;
-      location:string;
-      html_url:string;
-      
-  }
-    this.http.get<ApiResponse>("https://api.github.com/users/daneden?access_token"+ environment.apiKey ).subscribe(data=>{
-      this.user= new UserApi(data.name,data.followers,data.following,data.public_repospublic,data.location,data.avatar_url,data.html_url)
-   
-    },err=>{
-      this.user= new UserApi("Daniel Eden",0,0,0,"San Francisco, CA","https://avatars3.githubusercontent.com/u/439365?v=4","https://github.com/daneden")
-      console.log("Error occured ")
-  })
+  this.userService.userRequest()
+  this.user=this.userService.user
+
 }
 
 }
--- src/app/user-http/user-request.service.ts
+++ src/app/user-http/user-request.service.ts
@@ -7,37 +7,54 @@ import {UserApi} from '../user-api';
   providedIn: 'root'
 })
 export class UserRequestService {
+  user:UserApi;
 
-//   user:UserApi;
-
-//   constructor(private http:HttpClient) {
-//     this.user=new UserApi(0,0,0);
-//    }
-// }
-// interface ApiResponse{
-//   Repository:number;
-//   followers:number;
-//   following:number;
+  constructor(private http:HttpClient) {
+    this.user=new UserApi("",0,0,0,"","","");
+   }
   
-// }
-// let promise =new Promise((resolve,reject)=>{
-//   this.http.get<ApiResponse>(environment.apiKey).toPromise().then(response=>{
+
+userRequest(){
+
+interface ApiResponse{
+  avatar_url:string;
+  name:string;
+  public_repospublic:number;
+  followers:number;
+  following:number;
+  location:string;
+  html_url:string;
       
-//       this.user.Repository=response.Repository
-//       this.user.followers=response.followers
-//       this.user.following=response.following
-
-//       resolve()
-//   },
-//   error=>{
-//           this.user.Repository=0
-//           this.user.followers=0
-//           this.user.following=0
-//           reject(error)
-//       }
-//   )
-// })
-
-// return promise
-// }
 }
+let promise =new Promise((resolve,reject)=>{
+  this.http.get<ApiResponse>('https://api.github.com/users/daneden').toPromise().then(response=>{
+           
+  this.user.avatar_url=response.avatar_url
+  this.user.name=response.name
+  this.user.public_repospublic=response.public_repospublic
+  this.user.followers=response.followers
+  this.user.following=response.following
+  this.user.location=response.location
+  this.user.html_url=response.html_url
+       
+          
+       
+  resolve()
+},
+  error=>{
+    this.user.avatar_url=""
+    this.user.name=""
+    this.user.public_repospublic=0
+    this.user.followers=0
+    this.user.following=0
+    this.user.location=""
+    this.user.html_url=""
+    reject(error)
+  }
+)
+})
+
+return promise
+}
+}
+
