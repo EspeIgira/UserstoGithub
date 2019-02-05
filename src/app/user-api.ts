8dbbb479cb0a47a21590d00298c844109d5d7bde
EspeIgira <Mapapuro2>
2019-02-05 11:13:57 +0200

    updated

--- src/app/user-api.ts
+++ src/app/user-api.ts
@@ -1,4 +1,10 @@
 export class UserApi {
     
-    constructor(public name:string, public public_repospublic:number, followers:number, public following:number, public location:string,public avatar_url:string, public html_url:string){}
+    constructor(public name:string, 
+        public public_repospublic:number,
+        public followers:number, 
+        public following:number, 
+        public location:string,
+        public avatar_url:string, 
+        public html_url:string){}
 }
